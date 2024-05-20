c48f808f82c70660501d046532d140786bd5376a
Matheus Freitas <fernandes.matheus@aluno.uece.br>
2024-05-20 10:18:08 -0300

    Add SignUp component

---
+++ src/app/components/singUp.tsx
@@ -0,0 +1,25 @@
+// components/SignUp.tsx
+import { Box, Typography } from '@mui/material';
+import LoginOpt from './loginOpt'; 
+
+export default function SignUp() {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      width={'800px'}
+      minHeight="90vh"
+      bgcolor="inherit"
+      color="white"
+    >
+      <Typography variant="h2" className='text' gutterBottom style={{ fontWeight: "bold", width: "auto", fontFamily:"sans-serif"}}>
+        Happening now
+      </Typography>
+      <Typography variant="h5" gutterBottom>
+        Join today.
+      </Typography>
+      <LoginOpt/>
+    </Box>
+  );
+};
